539aa79386e7b4218ef373b80859b32c325203ea
amine <aminemalyana12@gmail.com>
2022-09-19 14:07:25 +0100

    modification

--- v2.c
+++ v2.c
@@ -10,35 +10,20 @@ float prix;
 } produit;
 
 produit pharx[]={
-			{123423,"produit1",88,19},
-			{857353,"produit2",154,1},
-			{508732,"produit3",82,1},
-			{839632,"produit4",98,10},
-			{256522,"produit5",188,9},
-			{284892,"produit6",8,49},
-			{109684,"produit7",84,38},
-			{188503,"produit8",78,54},
-			{128574,"produit9",103,23}
+			{123423,"aproduit1",88,19},
+			{857353,"dproduit2",154,1},
+			{508732,"fproduit3",82,1},
+			{839632,"cproduit4",98,10},
+			{256522,"bproduit5",188,9},
+			{284892,"iproduit6",8,49},
+			{109684,"yproduit7",84,38},
+			{188503,"zproduit8",78,54},
+			{128574,"xproduit9",103,23}
 };
 int length = (sizeof(pharx))/(sizeof(pharx[0]));
 
-//void ajouteProduit (int length) {
-//	printf("\tentrer le code de produit :");
-//	scanf("%d",&pharx[length].code);
-//
-//	printf("\tentree le nom de produit :");
-//	scanf("%s",&pharx[length].nom);
-//
-//	printf("\tentrer la quantite:");
-//	scanf("%d",&pharx[length].quantite);
-//
-//	printf("\tentrer le prix de produit :");
-//	scanf("%f",&pharx[length].prix);
-//
-//	++length;
-//	printf("\n\t le produit a ete ajuter\n\n");
-//}
-void listByprice (){
+
+/*void listByprice (){
     int j=0;
     int ech;
     produit tmp;
@@ -54,7 +39,25 @@ void listByprice (){
         }
         }
     }while(ech>0);
-}
+}*/
+
+/*void listByalphabet (){
+    int j=0;
+    int ech;
+    produit tmp;
+    do{
+        ech=0;
+        int i;
+        for(i=0;i<length-1;i++){
+            if(strcmp(pharx[i].nom,pharx[i+1].nom)>0){
+                tmp = pharx[i];
+                pharx[i]=pharx[i+1];
+                pharx[i+1]=tmp;
+            ech++;
+        }
+        }
+    }while(ech>0);
+}*/
 void acheterproduit (int cd,int length , int qt){
 
 	achep:
@@ -72,15 +75,6 @@ void acheterproduit (int cd,int length , int qt){
 			printf("quantite: %d \n",pharx[i].quantite);
 			break;
 		}
-	//	else if(pharx[i].code!=cd) {
-	//			printf("\n \t Il n y a pas de produit pour ce code");
-	//		goto achep;	}}
-
-	//	else { goto down;
-	//	}
-		//else {
-		//	printf("\n \t Il n y a pas de produit pour ce code");
-		//	goto achep;
 		}
 
 
@@ -99,11 +93,6 @@ void acheterproduit (int cd,int length , int qt){
 
 			printf("\n \t le produit a ete acheter");
 
-	//		down:
-	//			if(pharx[i].code!=cd){
-	//			printf("\n \t Il n y a pas de produit pour ce code");
-	//		goto achep;
-	//	}
 }
 
 
@@ -132,10 +121,6 @@ if(pharx[i].code!=cd1){
 			goto recherp;
 }
 }
-
-
-
-
 
 
 
@@ -299,17 +284,80 @@ int main() {
 			scanf("%d",&choix2);
 
 			if (choix2==1){
-				listByprice();
+				
+    int j=0;
+    int ech;
+    produit tmp;
+    do{
+        ech=0;
+        int i;
+        for(i=0;i<length-1;i++){
+            if(pharx[i].prix<pharx[i+1].prix){
+                tmp = pharx[i];
+                pharx[i]=pharx[i+1];
+                pharx[i+1]=tmp;
+            ech++;
+        }
+        }
+    }while(ech>0);
+				int i;
+					for (i=0;i<length;i++){
+			printf("code: %d \n",pharx[i].code);
+			printf("nom: %s \n",pharx[i].nom);
+			printf("prix: %.2f dh\n",pharx[i].prix);
+			printf("quantite: %d \n\n",pharx[i].quantite);
 			}
-			int i;
-			for (i=0;i<length;i++){
+			
+										rvalue5:  //checkpoint
+						printf("\nRetour au Menu ( y ) :");
+						scanf("%s" , &rvalue);
+							if(rvalue =='y')
+							{
+							  system("cls");
+							  goto depart;
+							}
+							else{
+							  goto rvalue5;
+							}
+			
+			}
+			
+			if (choix2==2){
+				    int j=0;
+    				int ech;
+    	produit tmp;
+    		do{
+        	ech=0;
+        		int i;
+        			for(i=0;i<length-1;i++){
+            if(strcmp(pharx[i].nom,pharx[i+1].nom)>0){
+                tmp = pharx[i];
+                pharx[i]=pharx[i+1];
+                pharx[i+1]=tmp;
+            ech++;
+        }
+        }
+    }while(ech>0);;
+								int i;
+					for (i=0;i<length;i++){
 			printf("code: %d \n",pharx[i].code);
 			printf("nom: %s \n",pharx[i].nom);
 			printf("prix: %.2f dh\n",pharx[i].prix);
 			printf("quantite: %d \n\n",pharx[i].quantite);
 			
 			}
-
+										rvalue4:  //checkpoint
+						printf("\nRetour au Menu ( y ) :");
+						scanf("%s" , &rvalue);
+							if(rvalue =='y')
+							{
+							  system("cls");
+							  goto depart;
+							}
+							else{
+							  goto rvalue4;
+							}
+			}
 
 
 			break;
@@ -356,6 +404,19 @@ int main() {
 							else{
 							  goto rvalue3;
 							}
+			break;
+		}
+		case 7:{
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			break;
 		}
 	}
